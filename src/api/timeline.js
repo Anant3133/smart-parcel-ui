e9d14e45a264e4e21a650f3432448b107bf51f65
Anant Mathur <anantmg10@gmail.com>
2025-06-27 10:55:10 +0530

    timeline issue fixed

--- src/api/timeline.js
+++ src/api/timeline.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { getToken } from '../utils/token';
 
-const BASE = `http://192.168.48.5:5180/api/Timeline`;
+const BASE = `http://${window.location.hostname}:5180/api/Timeline`;
 
 export async function fetchTimeline(trackingId) {
   const res = await axios.get(`${BASE}/${trackingId}`, {
